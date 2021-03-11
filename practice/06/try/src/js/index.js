a8d89cc6d9d897e126df2b49a4f15ec83812faf5
k-iwashita-prtimes <k-iwashita@M0004-prtadminnoMacBook-Pro.local>
2021-03-11 13:51:21 +0900

    6の課題完了

---
+++ practice/06/try/src/js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#form");
+  const genderText = document.querySelector("#gender-text");
+  const nameText = document.querySelector("#name-text");
+  const wrapper = document.querySelector("#text-wrapper");
+
+  form.button.addEventListener("click", () => {
+    wrapper.classList.add("is-active");
+    genderText.textContent = form.gender.value;
+    nameText.textContent = form.name.value;
+  });
+});
