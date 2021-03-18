e814fc49fb2f1c7485f00adc8651d65f44d0bd20
k-iwashita-prtimes <k-iwashita@M0004-prtadminnoMacBook-Pro.local>
2021-03-18 16:04:29 +0900

    ９の課題完了

---
+++ practice/09/try/src/js/index.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const MainImg = document.querySelector(".main-img");
+  const Items = document.querySelectorAll(".thumb-item");
+  const Imgs = [];
+  Items.forEach((el) => {
+    Imgs.push(el.querySelector(".thumb-item-img"));
+  });
+  Imgs.forEach((el) => {
+    el.addEventListener("mouseover", (event) => {
+      removeIsActive(Imgs);
+      addIsActive(event.target);
+      setMainTarget(MainImg, event.target);
+    });
+  });
+});
+
+function removeIsActive(imgs) {
+  imgs.forEach((e) => {
+    e.classList.remove("is-active");
+  });
+}
+
+function addIsActive(el) {
+  el.classList.add("is-active");
+}
+
+function setMainTarget(main, target) {
+  const mainSrc = target.getAttribute("src").replace("_small", "_large");
+  main.src = mainSrc;
+}
