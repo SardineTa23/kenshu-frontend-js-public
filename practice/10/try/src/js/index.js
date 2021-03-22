0402fe8210eb6ef5ecb1113f1db209a2ebb3f0d5
k-iwashita-prtimes <k-iwashita@M0004-prtadminnoMacBook-Pro.local>
2021-03-22 12:04:18 +0900

    10の課題完了

---
+++ practice/10/try/src/js/index.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const next = document.querySelector(".next");
+  const prev = document.querySelector(".prev");
+
+//   リストを取得
+  const list = document.getElementsByClassName("list")[0];
+
+//   nextをクリックした時
+  next.addEventListener("click", () => {
+    const firstItem = list.children[0];
+    changeOrder(firstItem, true);
+  });
+
+//   prevをクリックした時
+  prev.addEventListener("click", () => {
+    const lastItem = list.children[2];
+    changeOrder(lastItem, false);
+  });
+
+  function changeOrder(item, isNext) {
+    if (isNext === true) {
+        // 次の要素を取得し変数へ
+      const nextItem = list.children[1];
+    //   ポジションを打ち消すidを付与
+      nextItem.setAttribute("id", "reset-left");
+    //   transitionのための遅延処理
+      setTimeout(() => {
+        //   順番入れ替えÏ
+        list.appendChild(item);
+        // 打ち消しのためのidを外す
+        nextItem.setAttribute("id", "");
+      }, 300);
+    } else {
+      item.setAttribute("id", "reset-right");
+      setTimeout(() => {
+        list.insertBefore(item, list.children[0]);
+        item.setAttribute("id", "");
+      }, 300);
+    }
+  }
+});
